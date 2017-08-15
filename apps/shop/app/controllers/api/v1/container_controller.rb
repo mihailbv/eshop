9fd9678eb2ced2bf6c6111c90e9880b232acb49d
Mihail Badin <mihail@telmate.com>
2017-08-14 20:36:20 -0700

    update

--- apps/shop/app/controllers/api/v1/container_controller.rb
+++ apps/shop/app/controllers/api/v1/container_controller.rb
@@ -2,7 +2,19 @@ class Api::V1::ContainerController < ApplicationController
 	before_action :access_control_allow_origin
 
 	def index
-		render json: {status: :ok, products: [
+		render json: {
+					status: :ok, 
+					tags: [
+						{id: 1, title: "Пицца", image: "https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"},
+						{id: 2, title: "Салаты", image:"https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"},
+						{id: 3, title: "Супы", image:"https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"},
+						{id: 4, title: "Напитки", image:"https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"},
+						{id: 5, title: "Горячее", image:"https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"},
+						{id: 6, title: "Десерты", image:"https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"},
+						{id: 7, title: "WOK на сковороде", image:"https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"},
+						{id: 8, title: "Газировка и соки", image:"https://cdn2.iconfinder.com/data/icons/tasty-bites-icon-set/128/donuts.png"}
+					],
+					products: [
 						{id: 1, name: "Sushi", price: 140.5},
 						{id: 2, name: "Pizza", price: 100.0},
 						{id: 3, name: "Soup", price: 150.0},
